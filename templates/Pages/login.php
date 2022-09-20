e842b8b536c67d6bb4b629cee4242c375481fe06
Nack Thanaphon <78144742+nackdev2535@users.noreply.github.com>
2022-09-21 00:50:52 +0700

    first commit

---
+++ templates/Pages/login.php
@@ -0,0 +1,63 @@
+<div class="container-fluid">
+    <div class="row m-0 p-0 h-screen">
+        <nav class="d-flex justify-content-between mt-3">
+            <a>Document</a>
+            <div class="btn btn-success">สมัครสมาชิก</div>
+        </nav>
+        <div class="col-12 mt-4 text-center">
+            <h1 class="text-success fw-bold">Dog API</h1>
+            <h6>สมัครสมาชิกเพื่อรับสิทธิ์ใช้งาน Dog API
+                มากกว่า 100 สายพันธ์</h6>
+        </div>
+        <div class="col-12">
+            <div class="card  shadow-sm my-3 p-4">
+                <h1 class="text-center fw-bold">Login</h1>
+                <h6 class="text-center">เข้าสู่ระบบ</h6>
+                <form>
+                    <div class="mb-3">
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Your Email">
+                        <div id="emailHelp" class="form-text text-danger" hidden>We'll never share your email with anyone else.</div>
+                    </div>
+                    <div class="mb-3">
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Your Password">
+                    </div>
+                    <!-- <div class="mb-3 form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div> -->
+                </form>
+                <button id="submit" onclick="login()" class="btn btn-success w-100 rounded-pill">
+                    <h4>
+                        เข้าสู่ระบบ
+                    </h4>
+                </button>
+                <p class="m-0 pt-3 text-center">
+                    ต้องการ <a href="">สมัครสมาชิก ?</a>
+                </p>
+                <hr>
+                <p class="text-center mt-4">หรือ</p>
+                <div class="row m-0 p-0">
+                    <div class="col-sm-6 col-12">
+                        <button class="btn btn-primary  rounded-pill w-100 mb-2">Facebook</button>
+                    </div>
+                    <div class="col-sm-6 col-12">
+                        <button class="btn btn-danger  rounded-pill w-100 mb-2">Google</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    function login() {
+        //  let id = document.getElementById('submit')
+        Swal.fire({
+            icon: 'error',
+            title: 'เกิดข้อผิดพลาด',
+            text: 'ระบบไม่สามารถใช้ได้ในขณะนี้',
+            footer: '<a href="">ต้องการติดต่อผู้ดูแล ?</a>'
+        })
+    }
+</script>
---
+++ templates/layout/default.php
@@ -0,0 +1,47 @@
+<?php
+$cakeDescription = 'Dog API | Wellcome to Our world';
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?= $this->Html->charset() ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>
+        <?= $cakeDescription ?>:
+        <?= $this->fetch('title') ?>
+    </title>
+    <?= $this->Html->meta('icon') ?>
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
+
+    <?= $this->Html->css(['bootstrap/dist/css/bootstrap.min']) ?>
+
+    <?= $this->fetch('meta') ?>
+    <?= $this->fetch('css') ?>
+    <?= $this->fetch('script') ?>
+</head>
+
+<body>
+    <!-- <nav class="top-nav">
+        <div class="top-nav-title">
+            <a href="<?= $this->Url->build('/') ?>"><span>Cake</span>PHP</a>
+        </div>
+        <div class="top-nav-links">
+            <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/">Documentation</a>
+            <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
+        </div>
+    </nav> -->
+
+
+    <?= $this->Flash->render() ?>
+    <?= $this->fetch('content') ?>
+
+    <footer>
+    </footer>
+    <?= $this->Html->script('bootstrap.min'); ?>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+</body>
+
+</html>
